597d61d73536950a83dea9964e8aed9ec7119b9a
Rany Alo <rbge82@gmail.com>
2022-09-21 19:34:57 +0200

    Pagination

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,26 @@
 namespace App\Controller;
 
 use App\Repository\ArticlesRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use ContainerI39uXJ9\PaginatorInterface_82dac15;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ArticlesRepository $ArticlesRepository): Response
+    public function index(ArticlesRepository $ArticlesRepository, PaginatorInterface $paginator,
+                          Request $Request
+                        ): Response
     {
-        $articles = $ArticlesRepository->findAll();
+        $data = $ArticlesRepository->findAll();
+        $articles = $paginator->paginate(
+            $data,
+            $Request->query->getInt('page',1),3
+        );
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'articles' => $articles
